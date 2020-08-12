e93eb8eb818ddb40080bef197ad9bfee412ced36
shilerhobbs <shilerhobbs@gmail.com>
2020-08-11 21:25:35 -0700

    Added mor assests

---
+++ Assests/Map/Platformer.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="Platformer" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <image source="../PFantasy_SET1_v1.0/mainlevbuild.png" width="1024" height="1024"/>
+</tileset>
