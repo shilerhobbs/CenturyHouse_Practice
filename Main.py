ad51887fbad64a7c6c2801929eed7ffc866ae78e
stuff <blakegaarder@gmail.com>
2020-09-19 12:08:26 -0700

    edit player movement

--- Main.py
+++ Main.py
@@ -13,12 +13,30 @@ screen = pg.display.set_mode((640, 480))
 
 
 char_image = pg.image.load('Assests/Adventurer-1.5/Individual Sprites/adventurer-idle-01.png')
+charX = 0
+charY = 0
+
+
 
 
 running = True
 while running:
 
     for event in pg.event.get():
+        if event.type == pg.KEYDOWN:
+
+            if event.key == 273:
+                charY -= 2
+                print("up")
+            if event.key == 274:
+                charY += 2
+                print("down")
+            if event.key == 275:
+                charX += 2
+                print("right")
+            if event.key == 276:
+                charX -= 2
+                print("left")
 
         # The user closed the window!
         if event.type == pg.QUIT:
@@ -28,7 +46,8 @@ while running:
 
     # Logic goes here
 
-    screen.blit(char_image, (20, 20))
+    screen.fill((0, 0, 0))
+    screen.blit(char_image, (charX, charY))
 
     # Update Display
 
