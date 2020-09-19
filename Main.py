15f0bd0715fbefd05207bde56a27c99c7641de78
shilerhobbs <shilerhobbs@gmail.com>
2020-09-19 11:01:15 -0700

    Added boilerplate for launching point

--- Main.py
+++ Main.py
@@ -9,7 +9,11 @@ import pytmx, pyscroll
 pg.init()
 
 # Start the screen
-screen = pg.display.set_mode((640,480))
+screen = pg.display.set_mode((640, 480))
+
+
+char_image = pg.image.load('Assests/Adventurer-1.5/Individual Sprites/adventurer-idle-01.png')
+
 
 running = True
 while running:
@@ -23,6 +27,12 @@ while running:
             running = False
 
     # Logic goes here
+
+    screen.blit(char_image, (20, 20))
+
+    # Update Display
+
+    pg.display.flip()
 
 
 # Close the window
